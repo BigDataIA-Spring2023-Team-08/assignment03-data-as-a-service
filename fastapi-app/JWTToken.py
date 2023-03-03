f5527f48fc7e3b0202f1841b4da12f347925016d
Poojitha Konduparti <91445200+poojithakonduparti@users.noreply.github.com>
2023-03-03 13:57:14 -0600

    updated fast-api

--- fastapi-app/JWTToken.py
+++ fastapi-app/JWTToken.py
@@ -5,7 +5,7 @@ import schema
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 1
+ACCESS_TOKEN_EXPIRE_MINUTES = 3
 
 def create_access_token(data: dict):
     """Function to create a JWT Access token. It generates a token that expires after a 
@@ -41,6 +41,6 @@ def verify_token(token:str, credentials_exception):
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
-        token_data = schema.TokenData(username=username)
+        return schema.TokenData(username=username)
     except JWTError:
         raise credentials_exception
--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -6,9 +6,9 @@ import db_model
 from fastapi import FastAPI, APIRouter, status, HTTPException, Depends
 from dotenv import load_dotenv
 from sqlite3 import Connection
-from get_database_file import get_database_file
+from get_database_file import get_database_file, get_userdb_file
 from userdb import engine
-from routers import database, s3, fetchfile, user, authenticate
+from routers import database, s3, fetchfile, user, authenticate, logs
 
 #load env variables
 load_dotenv()
@@ -23,3 +23,4 @@ app.include_router(s3.router)
 app.include_router(fetchfile.router)
 app.include_router(user.router)
 app.include_router(authenticate.router)
+app.include_router(logs.router)
